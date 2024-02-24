4ccd68ad2f2b57f0d27072289868045c50264c1b
Ahsan Danish <a.danish@cequens.com>
2024-02-24 18:17:54 +0500

    interfaces and annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -16,7 +16,7 @@ class Car {
 
 }
 
-let car: Car = new Car();
+// let car: Car = new Car();
 
 // Object literal
 let point: {x: number, y: number} = {x: 10, y: 20}
---
+++ features/arrays.ts
@@ -0,0 +1,25 @@
+const carMaker = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+];
+
+// Help with inference when extracting values
+const car = carMaker[0];
+const myCar = carMaker.pop();
+
+// Prevent incompatible values
+// carMaker.push(100);
+
+// Help with 'map'
+carMaker.map((car: string): string => {
+    return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10'];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
